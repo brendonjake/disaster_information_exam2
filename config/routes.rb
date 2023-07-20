5c23e55a97d0975d82e40c7b08a188a18fa563eb
brendonjake <macapaz.b1998@gmail.com>
2023-07-20 16:24:07 +0800

    Add create post

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
 
   resources :categories, except: :show
 
+  resources :posts
+
   namespace :api do
     namespace :v1 do
       resources :regions, only: %i[index show], defaults: { format: :json } do
