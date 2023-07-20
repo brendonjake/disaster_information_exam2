604301a223a4ce46786c7d347e5f153f7935ad24
brendonjake <macapaz.b1998@gmail.com>
2023-07-20 14:19:57 +0800

    Created routes and controller for regions and provinces

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,14 @@ Rails.application.routes.draw do
   root 'welcome#index'
 
   resources :categories, except: :show
- end
+
+  namespace :api do
+    namespace :v1 do
+      resources :regions, only: %i[index show], defaults: { format: :json } do
+        resources :provinces, only: :index, defaults: { format: :json }
+      end
+
+      resources :provinces, only: %i[index show], defaults: { format: :json }
+    end
+  end
+end
