6c188a5c7b721b06bd25ea6ed37170f8e5f3a66e
brendonjake <macapaz.b1998@gmail.com>
2023-07-20 18:50:19 +0800

    Adding post categoryship

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
 
   def index
-    @posts = Post.all
+    @posts = Post.includes(:categories).all
   end
 
   def new
@@ -47,6 +47,6 @@ class PostsController < ApplicationController
   end
 
   def post_params
-    params.require(:post).permit(:title, :content, :address)
+    params.require(:post).permit(:title, :content, :address, category_ids: [])
   end
 end
